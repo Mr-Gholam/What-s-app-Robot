b081740581b45573ccb49cfacedd50039d1fc9b0
Mr Gholam <mobingh093@gmail.com>
2022-09-04 15:32:37 +0430

    proper database structure added

---
+++ database.js
@@ -0,0 +1,76 @@
+import Sequelize from "sequelize"
+
+
+export default class database {
+    constructor(password, host) {
+        this.password = password
+        this.host = host
+        this.db = null
+        this.message = null
+        this.contacts = null
+    }
+    async start() {
+        this.db = new Sequelize('whatsapp', 'root', this.password, {
+            host: this.host,
+            dialect: 'mysql',
+            logging: false
+        })
+        this.message = this.db.define('Messages', {
+            id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false
+            },
+            number: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            msgContent: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            timestamp: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            }
+        })
+        this.contacts = this.db.define('Contacts', {
+            id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            numbers: {
+                type: Sequelize.STRING,
+                allowNull: false
+            }
+        })
+        try {
+            await this.db.authenticate();
+            console.log('Connection has been established successfully.');
+            await this.db.sync({ force: true });
+            console.log('database has been synced')
+        } catch (error) {
+            console.error('Unable to connect to the database:', error);
+        }
+    }
+    async createMessage(mobile, msgText, date) {
+        await this.message.create({
+            number: mobile,
+            msgContent: msgText,
+            timestamp: date
+        })
+    }
+    async createContact(name, numbers) {
+        await this.contacts.create({
+            name: name,
+            numbers: numbers
+        })
+    }
+}
--- index.js
+++ index.js
@@ -1,60 +1,129 @@
-const qrcode = require('qrcode-terminal');
-const env = require('dotenv')
-const TelegramBot = require('node-telegram-bot-api')
-const { Client } = require('whatsapp-web.js');
+// const qrcode = require('qrcode-terminal');
+// const env = require('dotenv')
+// const TelegramBot = require('node-telegram-bot-api')
+// const { Client } = require('whatsapp-web.js');
+// const Database = require('./database')
+// var vcard = require('vcard-json');
+import qrcode from 'qrcode-terminal'
+import env from 'dotenv'
+import TelegramBot from 'node-telegram-bot-api'
+import { Client } from 'whatsapp-web.js'
+import Database from './database.js';
+import vcard from 'vcard-json'
+
+
+env.config()
 const client = new Client({});
 
 
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('whatsapp', 'root', 'mehdi007', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false
-})
-env.config()
+const database = new Database('mehdi007', 'localhost')
 
 const token = process.env.TOKEN
 const chatId = process.env.CHATID
 
-const bot = new TelegramBot(token, { polling: true });
+// const bot = new TelegramBot(token, { polling: true });
 
-const Msg = sequelize.define('Messages', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false
-    },
-    number: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    msgContent: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    timestamp: {
-        type: Sequelize.INTEGER,
-        allowNull: false
+await database.start()
+
+
+vcard.parseVcardFile('./contacts.vcf', async function (err, data) {
+    // await database.sync(true)
+    if (err) console.log('oops:' + err);
+    else {
+        for (let index = 0; index < data.length; index++) {
+            const element = data[index];
+            const name = element.fullname
+            let numbers
+            for (let j = 0; j < element.phone.length; j++) {
+                const number = element.phone[j];
+                // getting valid number
+                if (number.value.length == 13) {
+                    // removing - from number
+                    if (number.value[3] == '-') {
+                        const correctNumber = number.value.split('-')
+                        if (numbers) {
+                            numbers = numbers + '|' + correctNumber.join('')
+                        } else {
+                            numbers = correctNumber.join('')
+                        }
+                        continue
+                    }
+                    // removing +98 from number and adding 0
+                    let correctNumber = number.value.slice(3, 13)
+                    // adding more numbers to a contact
+                    if (numbers) {
+                        numbers = numbers + '|' + 0 + correctNumber
+                    } else {
+                        numbers = 0 + correctNumber
+                    }
+                    continue
+                }
+                // removing invalid number
+                if (number.value.length <= 13) {
+                    continue
+                }
+                // filtering for more valid number
+                if (number.value.length >= 13) {
+                    // filtering for more iranian numbers
+                    if (number.value.includes(98)) {
+                        // iranian numbers
+                        if (number.value.slice(0, 3) == '+98') {
+                            // invalid number removed 
+                            if (number.value.length - 3 !== 10) continue
+                            let correctNumber = number.value.slice(3)
+                            // adding more numbers to a contact
+                            if (numbers) {
+                                numbers = numbers + '|' + 0 + correctNumber
+                            } else {
+                                numbers = 0 + correctNumber
+                            }
+                        }
+                        if (number.value.slice(0, 2) == 98) {
+                            const shortedNumber = number.value.slice(2)
+                            const correctNumber = shortedNumber.split('-')
+                            // adding more numbers to a contact
+                            if (numbers) {
+                                numbers = numbers + '|' + 0 + correctNumber.join('')
+                            } else {
+                                numbers = 0 + correctNumber.join('')
+                            }
+                            continue
+                        }
+                        //  Not iranian number 
+                        if (numbers) {
+                            numbers = numbers + '|' + number.value
+                        } else {
+                            numbers = number.value
+                        }
+                    } else {
+                        //  Not iranian number 
+                        // adding more numbers to a contact
+                        if (numbers) {
+                            numbers = numbers + '|' + number.value
+                        } else {
+                            numbers = number.value
+                        }
+                    }
+                }
+            }
+            // preventing empty number to be inserted to database
+            if (!numbers) continue
+            database.createContact(name, numbers)
+        }
     }
-})
-
-
-
-client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', async () => {
-    console.log('ready')
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-});
+
+
+
+
+// client.on('qr', qr => {
+//     qrcode.generate(qr, { small: true });
+// });
+
+// client.on('ready', async () => {
+//     console.log('ready')
+// });
 
 client.on('message', async (message) => {
     const mobile = message.from.split('@')[0]
@@ -69,21 +138,17 @@ client.on('message', async (message) => {
 
     if (message._data.quotedMsg) reply = message._data.quotedMsg.body
 
-    await Msg.create({
-        number: mobile,
-        msgContent: msgText,
-        timestamp: date
-    })
-    try {
-        if (reply) return bot.sendMessage(chatId, `${msgText} \n\n ${reply} \n\n  ${callableNumber} \n${name} `)
-        bot.sendMessage(chatId, `${msgText}  \n\n  ${callableNumber} \n${name} `)
-    } catch (err) {
-        console.log('telegram' + err.message)
-    }
+
+    database.createMessage(callableNumber, msgText, date)
+
+    // try {
+    //     if (reply) return bot.sendMessage(chatId, `${msgText} \n\n ${reply} \n\n  ${callableNumber} \n${name} `)
+    //     bot.sendMessage(chatId, `${msgText}  \n\n  ${callableNumber} \n${name} `)
+    // } catch (err) {
+    //     console.log('telegram' + err.message)
+    // }
 }
 
 );
-(async () => { await sequelize.sync({ force: false }); })()
-
 
 client.initialize();
